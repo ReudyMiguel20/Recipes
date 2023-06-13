8a5dc9704b6f453f4a7bb51d29e7fca5a3a64eba
Reudy Guerrero <87297026+ReudyMiguel20@users.noreply.github.com>
2023-06-13 13:56:19 -0400

    Update RecipeController.java

--- Recipes/task/src/recipes/Rest/RecipeController.java
+++ Recipes/task/src/recipes/Rest/RecipeController.java
@@ -1,38 +1,38 @@
-package recipes.Rest;
+// package recipes.Rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import recipes.BusinessLogic.Recipe;
-import recipes.BusinessLogic.RecipeStorage;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import recipes.BusinessLogic.Recipe;
+// import recipes.BusinessLogic.RecipeStorage;
 
-@RestController
-@RequestMapping("/api/recipe")
-public class RecipesRest {
+// @RestController
+// @RequestMapping("/api/recipe")
+// public class RecipesRest {
 
-    private RecipeStorage recipeStorage;
+//     private RecipeStorage recipeStorage;
 
-    @Autowired
-    public RecipesRest(RecipeStorage recipeStorage) {
-        this.recipeStorage = recipeStorage;
-    }
+//     @Autowired
+//     public RecipesRest(RecipeStorage recipeStorage) {
+//         this.recipeStorage = recipeStorage;
+//     }
 
-    @PostMapping("/new")
-    public ResponseEntity<?> addRecipe(@RequestBody Recipe recipe) {
-        this.recipeStorage.addRecipe(recipe);
-        return ResponseEntity.ok().body("{\n \"id\": " + this.recipeStorage.getId() + "\n}");
-    }
+//     @PostMapping("/new")
+//     public ResponseEntity<?> addRecipe(@RequestBody Recipe recipe) {
+//         this.recipeStorage.addRecipe(recipe);
+//         return ResponseEntity.ok().body("{\n \"id\": " + this.recipeStorage.getId() + "\n}");
+//     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> getRecipe(@PathVariable int id) {
-        Recipe checkRecipe = null;
-        checkRecipe = this.recipeStorage.getRecipeById(id);
+//     @GetMapping("/{id}")
+//     public ResponseEntity<?> getRecipe(@PathVariable int id) {
+//         Recipe checkRecipe = null;
+//         checkRecipe = this.recipeStorage.getRecipeById(id);
 
-        if (checkRecipe == null) {
-            return ResponseEntity.status(404).build();
-        } else {
-            return ResponseEntity.ok().body(this.recipeStorage.getRecipeById(id));
-        }
-    }
+//         if (checkRecipe == null) {
+//             return ResponseEntity.status(404).build();
+//         } else {
+//             return ResponseEntity.ok().body(this.recipeStorage.getRecipeById(id));
+//         }
+//     }
 
-}
+// }
